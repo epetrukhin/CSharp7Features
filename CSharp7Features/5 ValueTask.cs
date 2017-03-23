7b45549d7b7d36503ef1ce428a1d444fd45e3e64
petrukhin <petrukhin@skbkontur.ru>
2017-03-23 15:27:29 +0300

    Value Task

---
+++ CSharp7Features/5 ValueTask.cs
@@ -0,0 +1,36 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+// ReSharper disable ArrangeMethodOrOperatorBody
+// ReSharper disable UnusedParameter.Local
+#pragma warning disable 1998
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharp7Features
+{
+	internal sealed class Value_Task
+	{
+		// Нужен NuGet пакет System.Threading.Tasks.Extensions
+
+		private readonly Dictionary<string, int> cache = new Dictionary<string, int>();
+
+		public async ValueTask<int> GetDataCSharp7(string from)
+		{
+			int result;
+			if (cache.TryGetValue(from, out result))
+				return result;
+
+			result = await GetDataCore(from);
+			cache.Add(from, result);
+			return result;
+		}
+
+		private static async Task<int> GetDataCore(string from)
+		{
+			// Здесь мы что-то откуда-то достаём
+
+			return 42;
+		}
+	}
+}
