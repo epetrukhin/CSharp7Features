c6e5e0fbba331bae4c43958c47cd774365100b9f
petrukhin <petrukhin@skbkontur.ru>
2017-03-23 14:06:59 +0300

    Expression Bodied Members

---
+++ CSharp7Features/4 Expression Bodied Members.cs
@@ -0,0 +1,42 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable ConvertToAutoPropertyWhenPossible
+
+namespace CSharp7Features
+{
+	internal sealed class ExpressionBodiedMembers
+	{
+		private int value;
+
+		#region Появилось в C# 6
+		public bool IsPositive => value > 0;
+
+		public override string ToString() => value.ToString();
+
+		public static bool operator ==(ExpressionBodiedMembers left, ExpressionBodiedMembers right) => Equals(left, right);
+
+		public static bool operator !=(ExpressionBodiedMembers left, ExpressionBodiedMembers right) => !Equals(left, right);
+		#endregion
+
+		#region Появилось в C# 7
+		public ExpressionBodiedMembers(int value) => this.value = value;
+
+		~ExpressionBodiedMembers() => value = 0;
+
+		public int Value
+		{
+			get => value;
+			set => this.value = value;
+		}
+
+		public int GetOnly
+		{
+			get => value;
+		}
+
+		public int SetOnly
+		{
+			set => this.value = value;
+		}
+		#endregion
+	}
+}
