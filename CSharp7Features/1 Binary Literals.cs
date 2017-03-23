bee28a7ddf9dae880ff3783039372f7b88ac50ad
petrukhin <petrukhin@skbkontur.ru>
2017-03-23 13:01:58 +0300

    Binary literals + Digit separator

---
+++ CSharp7Features/1 Binary Literals.cs
@@ -0,0 +1,44 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
+#pragma warning disable 219
+using System;
+
+namespace CSharp7Features
+{
+	internal static class BinaryLiterals
+	{
+		private static void Demo()
+		{
+			// Литералы для чисел до C#7
+			var aDec = 123456;
+			var aHex = 0X123456;
+			var bDec = 123456U;
+			var bHex = 0X123456U;
+			var cDec = 123456L;
+			var cHex = 0X123456L;
+			var dDec = 123456UL;
+			var dHex = 0X123456UL;
+
+			var e = 123.456D;
+			var f = 123.456F;
+			var g = 123.456M;
+
+			// Двоичные литералы
+			var aBin = 0b11110001001000000;
+			var bBin = 0b11110001001000000U;
+			var cBin = 0b11110001001000000L;
+			var dBin = 0b11110001001000000UL;
+		}
+
+		[Flags]
+		private enum Foo
+		{
+			None  = 0b00000, //  0
+			Flag1 = 0b00001, //  1
+			Flag2 = 0b00010, //  2
+			Flag3 = 0b00100, //  4
+			Flag4 = 0b01000, //  8
+			Flag5 = 0b10000  // 16
+		}
+	}
+}
---
+++ CSharp7Features/2 Digit Separator.cs
@@ -0,0 +1,21 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
+#pragma warning disable 219
+
+namespace CSharp7Features
+{
+	internal static class DigitSeparator
+	{
+		private static void Demo()
+		{
+			var a = 123_456;
+			var b = 0XDead_BeefUL;
+			var e = 0b1001_0110;
+			var f = 123_456.789_123D;
+			var g = 123_456.789_123F;
+			var h = 123_456.789_123M;
+
+			// Разделитель цифр можно ставить между цифрами в любом месте в любом количестве
+		}
+	}
+}
