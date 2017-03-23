63114927bb8475924a2b67e11e64a460398748cf
petrukhin <petrukhin@skbkontur.ru>
2017-03-23 13:55:23 +0300

    Throw expressions

---
+++ CSharp7Features/3 Throw Expressions.cs
@@ -0,0 +1,29 @@
+﻿// ReSharper disable ArrangeConstructorOrDestructorBody
+// ReSharper disable ArrangeMethodOrOperatorBody
+// ReSharper disable NotAccessedField.Local
+// ReSharper disable UnusedMember.Global
+
+using System;
+using System.Collections.Generic;
+
+namespace CSharp7Features
+{
+	internal sealed class ThrowExpressions
+	{
+		private readonly string value;
+
+		public ThrowExpressions(string value)
+		{
+			this.value = value ?? throw new ArgumentNullException(nameof(value));
+		}
+
+		public T GetSecond<T>(IReadOnlyList<T> list)
+		{
+			return list.Count >= 2
+				? list[1]
+				: throw new ArgumentException();
+		}
+
+		public void NotImplemented() => throw new NotImplementedException();
+	}
+}
