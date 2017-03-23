1898cd6bfb00d5871937d7c5bb7ae6f78a5797f0
petrukhin <petrukhin@skbkontur.ru>
2017-03-23 15:27:49 +0300

    Ref return and ref locals

---
+++ CSharp7Features/6 Ref Returns and Locals.cs
@@ -0,0 +1,69 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
+
+using System;
+using System.Collections.Generic;
+
+namespace CSharp7Features
+{
+	internal static class RefReturnsAndLocals
+	{
+		private static int FindIndex<T>(T value, T[] data)
+		{
+			var comparer = EqualityComparer<T>.Default;
+
+			for (var i = 0; i < data.Length; i++)
+			{
+				var item = data[i];
+				if (comparer.Equals(item, value))
+					return i;
+			}
+
+			throw new IndexOutOfRangeException();
+		}
+
+		private static void CSharp6()
+		{
+			var data = new[] { "1", "2", "3" };
+			var index = FindIndex("2", data);
+			data[index] = "42"; // data: { 1, 42, 3 }
+		}
+
+		private static ref T Find<T>(T value, T[] data)
+		{
+			var comparer = EqualityComparer<T>.Default;
+
+			for (var i = 0; i < data.Length; i++)
+			{
+				ref var item = ref data[i];
+				if (comparer.Equals(item, value))
+					return ref item;
+			}
+
+			throw new IndexOutOfRangeException();
+		}
+
+		public static void CSharp7()
+		{
+			var data = new[] { "1", "2", "3" };
+			ref var index = ref Find("2", data); // Если убрать оба ref, то работать не будет
+			index = "42"; // data: { 1, 42, 3 }
+		}
+
+		private static ref int DoNotCompiled()
+		{
+			var x = 42;
+			return ref x;
+
+//			var xs = new[] { 1, 2, 3 };
+//			return ref xs[1];
+		}
+
+		private sealed class Foo
+		{
+			private /*readonly*/ bool field;
+
+			public ref bool GetField() => ref field;
+		}
+	}
+}
